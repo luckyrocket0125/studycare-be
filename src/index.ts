dcbe44566fb7859c75446dc64ae7af016cbb009f
unknown <seagulls.lucky125@gmail.com>
2025-11-23 23:49:40 -0500

    fixed cors 6

--- src/index.ts
+++ src/index.ts
@@ -30,12 +30,15 @@ app.use(compression());
 //   credentials: true
 // }));
 app.use(cors());
-// app.use(cors({
-//   origin: true,
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-// }));
+app.use(cors({
+  origin: ["https://studycare-qb2az5uic-seniors-projects-b89c90fe.vercel.app", "https://studycare-fe.vercel.app"],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
+
+app.options("*", cors());
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
