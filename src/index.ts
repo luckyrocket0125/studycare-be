463e71f7620b0a722f61d423216c10a4ba18f36f
unknown <seagulls.lucky125@gmail.com>
2025-11-23 22:58:24 -0500

    cors fixed 1

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,10 @@ app.use(compression());
 //   credentials: true
 // }));
 app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
