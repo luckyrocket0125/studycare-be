ab4eca73689f5eab57ec07e7ac9e1ee74f1b401f
unknown <seagulls.lucky125@gmail.com>
2025-11-23 23:51:01 -0500

    fixed cors 7

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(cors({
   origin: ["https://studycare-qb2az5uic-seniors-projects-b89c90fe.vercel.app", "https://studycare-fe.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Access-Control-Allow-Headers']
 }));
 
 app.options("*", cors());
